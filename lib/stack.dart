70e2bf69fe792aed0e1a924b461a4a6016720286
dksadasjkl <ckdgus6038@naver.com>
2025-04-29 22:19:53 +0900

    feat : 버튼 만들기

--- lib/stack.dart
+++ lib/stack.dart
@@ -43,16 +43,38 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Test Title ^^"), // 상단 앱 바의 제목
       ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () => print("GestureDetector used"),
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.amber,
-          ),
-        )
-      )
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.yellow,
+        child: Stack(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.black,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 50, left: 50),
+              color: Colors.red,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 100, left: 100),
+              color: Colors.green,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              margin: const EdgeInsets.only(top: 150, left: 150),
+              color: Colors.orange,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
