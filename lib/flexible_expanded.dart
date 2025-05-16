deaa400f58a1aeced06f5885faac0c435a599a99
dksadasjkl <ckdgus6038@naver.com>
2025-05-16 19:23:30 +0900

    Flexible, Expanded 사용하여 반응형

---
+++ lib/flexible_expanded.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test Title"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
