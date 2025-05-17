7ba50d4616506dc9729393d2716e1ce19317088b
dksadasjkl <ckdgus6038@naver.com>
2025-05-17 22:51:30 +0900

    Input 사용하기

---
+++ lib/Input.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  late SharedPreferences _prefs;
+  String _username = "";
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getUsername();
+  }
+
+  _savaUsername() {
+    setState(() {
+      _username = _usernameController.text;
+      _prefs.setString("currentUsername", _username);
+    });
+  }
+
+  _getUsername() async {
+    _prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _username = _prefs.getString("currentUsername") ?? "";
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test Title"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Text("현재 사용자 이름: $_username"),
+            Container(
+              child: TextField(
+                controller: _usernameController,
+                textAlign: TextAlign.left,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Input your username',
+                ),
+              ),
+            ),
+            TextButton(onPressed: () => _savaUsername(), child: const Text("저장"))
+          ],
+        ),
+      )
+    );
+  }
+}
